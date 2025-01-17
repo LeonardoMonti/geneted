0171c4817e1115790e02362b041790dafaac6f2c
Leonardo Monti <contatoleomonti@gmail.com>
2025-01-17 00:39:55 -0300

    segunda levra

---
+++ testing.js
@@ -0,0 +1,23 @@
+const data17012025 = {
+  "nqgc": "nqgc14, nqgc05, nqgc30, nqgc88, nqgc07, nqgc04, nqgc69, nqgc41, nqgc29, nqgc10",
+  "ysrx": "ysrx62, ysrx94, ysrx58, ysrx21, ysrx02, ysrx80, ysrx83, ysrx78, ysrx39, ysrx84",
+  "ukpk": "ukpk50, ukpk04, ukpk40, ukpk74, ukpk23, ukpk63, ukpk12, ukpk08, ukpk97, ukpk41",
+  "snaj": "snaj62, snaj76, snaj82, snaj42, snaj30, snaj86, snaj61, snaj64, snaj37, snaj56",
+  "rbfa": "rbfa02, rbfa40, rbfa01, rbfa88, rbfa26, rbfa74, rbfa14, rbfa71, rbfa13, rbfa73",
+  "yrtb": "yrtb43, yrtb12, yrtb54, yrtb90, yrtb37, yrtb74, yrtb96, yrtb10, yrtb04, yrtb07",
+  "mutz": "mutz55, mutz85, mutz21, mutz83, mutz84, mutz24, mutz10, mutz69, mutz95, mutz97",
+  "dhty": "dhty84, dhty38, dhty50, dhty14, dhty49, dhty79, dhty64, dhty88, dhty65, dhty78",
+  "zzth": "zzth90, zzth78, zzth43, zzth46, zzth84, zzth10, zzth51, zzth28, zzth59, zzth21",
+}
+
+const data17012025_02 = {
+  "bxeh": "bxeh07, bxeh11, bxeh97, bxeh82, bxeh28, bxeh33, bxeh77, bxeh08, bxeh48, bxeh50",
+  "qykz": "qykz32, qykz15, qykz54, qykz50, qykz78, qykz79, qykz26, qykz65, qykz55, qykz01",
+  "uycc": "uycc44, uycc11, uycc41, uycc46, uycc65, uycc67, uycc85, uycc21, uycc28, uycc27",
+  "aerv": "aerv27, aerv65, aerv81, aerv59, aerv80, aerv76, aerv02, aerv37, aerv95, aerv70",
+  "uynq": "uynq71, uynq30, uynq46, uynq62, uynq26, uynq98, uynq93, uynq29, uynq96, uynq90",
+  "tnqe": "tnqe38, tnqe42, tnqe08, tnqe41, tnqe82, tnqe09, tnqe25, tnqe46, tnqe24, tnqe35",
+  "dsys": "dsys16, dsys30, dsys89, dsys76, dsys69, dsys84, dsys33, dsys77, dsys24, dsys60",
+  "oxud": "oxud70, oxud63, oxud24, oxud09, oxud29, oxud64, oxud96, oxud32, oxud93, oxud23",
+  "aucs": "aucs15, aucs40, aucs50, aucs21, aucs88, aucs63, aucs68, aucs65, aucs45, aucs69"
+}
